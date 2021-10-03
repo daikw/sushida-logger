62471ee56a56545cf288587f89a1fb8c04e3209e
daikw <hazuhi@gmail.com>
2021-10-03 11:54:43 +0900

    :+1: migrate manifest version 2 to 3 / and add command shortcut

--- src/background.js
+++ src/background.js
@@ -19,3 +19,15 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     })
   }
 })
+
+chrome.action.onClicked.addListener((tab) => {
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    func: contentScriptFunc,
+    args: ["action"],
+  })
+})
+
+function contentScriptFunc(name) {
+  console.log(`"${name}" executed`)
+}
